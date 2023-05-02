e9e82bcc98ba084a14f0d810f25916a88a1d46b4
Min A <min2010011@gmail.com>
2023-05-02 21:29:53 +0900

    🍻congratulation!
    Init project

---
+++ hanatour/src/main/java/efub/clone/hanatour/HanatourApplication.java
@@ -0,0 +1,13 @@
+package efub.clone.hanatour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanatourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanatourApplication.class, args);
+	}
+
+}
---
+++ hanatour/src/test/java/efub/clone/hanatour/HanatourApplicationTests.java
@@ -0,0 +1,13 @@
+package efub.clone.hanatour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanatourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
