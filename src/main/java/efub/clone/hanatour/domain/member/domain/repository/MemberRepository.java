619ddfdb0753da8497085095ea812487f8306d86
aoqlsdl <bini92444@gmail.com>
2023-05-07 23:54:33 +0900

    ✨feat: Member Repository 추가

---
+++ src/main/java/efub/clone/hanatour/domain/member/domain/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package efub.clone.hanatour.domain.member.domain.repository;
+
+import efub.clone.hanatour.domain.member.domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByAccount(String account);
+    boolean existsByAccount(String account);
+}
