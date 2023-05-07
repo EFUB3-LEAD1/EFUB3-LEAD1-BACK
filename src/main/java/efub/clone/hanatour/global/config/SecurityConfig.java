99d5b21244d5bed656b95c0e0969140c07355b74
aoqlsdl <bini92444@gmail.com>
2023-05-08 00:36:38 +0900

    ✨feat: Jwt Security 설정

--- src/main/java/efub/clone/hanatour/global/config/SecurityConfig.java
+++ src/main/java/efub/clone/hanatour/global/config/SecurityConfig.java
@@ -1,26 +1,44 @@
 package efub.clone.hanatour.global.config;
 
+import efub.clone.hanatour.global.jwt.JwtAccessDeniedHandler;
+import efub.clone.hanatour.global.jwt.JwtAuthenticationEntryPoint;
+import efub.clone.hanatour.global.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity.csrf().disable()
                 .headers().frameOptions().disable()
+                .and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).accessDeniedHandler(jwtAccessDeniedHandler)
+
                 .and().authorizeHttpRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/**").permitAll()
-                .anyRequest().permitAll();
+                .anyRequest().permitAll()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//                .and().apply(new JwtSecurityConfig(tokenProvider));
 
         return httpSecurity.build();
     }
