044552ad1d4af5dcde9fe79a3bd2a9aa90b154f4
aoqlsdl <bini92444@gmail.com>
2023-05-11 23:25:16 +0900

    ✨feat: accessToken 남은 시간 메서드 추가

--- src/main/java/efub/clone/hanatour/global/jwt/JwtSecurityConfig.java
+++ src/main/java/efub/clone/hanatour/global/jwt/JwtSecurityConfig.java
@@ -13,7 +13,7 @@ public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurity
     // TokenProvider 를 주입받아서 JwtFilter 를 통해 Security 로직에 필터를 등록
     @Override
     public void configure(HttpSecurity http) {
-        JwtFilter customFilter = new JwtFilter(tokenProvider);
-        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+//        JwtFilter customFilter = new JwtFilter(tokenProvider);
+//        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
