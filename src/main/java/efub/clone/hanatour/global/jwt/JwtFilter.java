bee165f1d273917fd0a0e2dcdc32c856a96db0b3
aoqlsdl <bini92444@gmail.com>
2023-05-11 23:25:47 +0900

    ✨feat: 로그아웃 메서드 추가

--- src/main/java/efub/clone/hanatour/global/jwt/JwtFilter.java
+++ src/main/java/efub/clone/hanatour/global/jwt/JwtFilter.java
@@ -1,9 +1,11 @@
 package efub.clone.hanatour.global.jwt;
 
+import efub.clone.hanatour.domain.member.domain.dto.TokenRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -13,6 +15,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
@@ -55,5 +58,27 @@ public class JwtFilter extends OncePerRequestFilter {
             return bearerToken.substring(7);
         }
         return null;
+    }
+
+    @Transactional
+    public void logout(TokenRequestDto tokenRequestDto){
+        // 로그아웃 하고 싶은 토큰이 유효한 지 먼저 검증하기
+        if (!tokenProvider.validateToken(tokenRequestDto.getAccessToken())){
+            throw new IllegalArgumentException("로그아웃 : 유효하지 않은 토큰입니다.");
+        }
+
+        // Access Token에서 User email을 가져온다
+        Authentication authentication = tokenProvider.getAuthentication(tokenRequestDto.getAccessToken());
+
+        // Redis에서 해당 User email로 저장된 Refresh Token 이 있는지 여부를 확인 후에 있을 경우 삭제를 한다.
+        if (redisTemplate.opsForValue().get("RT:"+authentication.getName())!=null){
+            // Refresh Token을 삭제
+            redisTemplate.delete("RT:"+authentication.getName());
+        }
+
+        // 해당 Access Token 유효시간을 가지고 와서 BlackList에 저장하기
+        Long expiration = tokenProvider.getExpiration(tokenRequestDto.getAccessToken());
+        redisTemplate.opsForValue().set(tokenRequestDto.getAccessToken(),"logout", expiration, TimeUnit.MILLISECONDS);
+
     }
 }
