a5977e767cd14a8153d26f45378a4c6eac71cbdb
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-05-19 18:35:24 +0900

    💬 chore: swagger 요청 url 설정 완료

--- src/main/java/efub/clone/hanatour/HanatourApplication.java
+++ src/main/java/efub/clone/hanatour/HanatourApplication.java
@@ -1,10 +1,13 @@
 package efub.clone.hanatour;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@OpenAPIDefinition(servers = {@Server(url = "https://api.htour.xyz", description = "배포된 서버")})
 @SpringBootApplication
 public class HanatourApplication {
 
